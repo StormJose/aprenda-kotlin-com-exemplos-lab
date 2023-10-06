84e1a1a5a6635d3ab72b8712525cf4f407ed12e3
StormJose <ricardo23feitosa@gmail.com>
2023-10-05 21:42:04 -0300

    initial commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,52 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: MutableList<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos: MutableList<Usuario> = mutableListOf()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+       inscritos.add(usuario)
+       val informacaoAluno = usuario.toString()
+        println("$informacaoAluno matriculado(a) em $nome. Alunos matriculados: $inscritos")
     }
+     
+    fun mostrarConteudos() {
+        val numConteudos = conteudos.size
+        
+        println("$nome contem ao todo $numConteudos conteudo(s), sendo eles: $conteudos")
+        
+    }
+    
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+val programacaoWeb = Formacao("Programação Web", mutableListOf(ConteudoEducacional("Redes")))
+
+val pedro = Usuario("Pedro", 19)
+val antonio = Usuario("Antônio", 21)
+
+
+programacaoWeb.matricular(pedro)
+programacaoWeb.matricular(antonio)
+
+
+
+val Python = ConteudoEducacional("Python", 10)
+val redesNeurais = ConteudoEducacional("Redes Neurais", 15)
+
+val iA = Formacao("Inteligência Artificial", mutableListOf(Python, redesNeurais))
+
+
+
+iA.matricular(Usuario("Sabrina", 30))
+iA.matricular(Usuario("José", 32))
+
+programacaoWeb.mostrarConteudos()
+iA.mostrarConteudos()
+
 }
